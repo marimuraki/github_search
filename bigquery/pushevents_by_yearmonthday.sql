1ecbba0c0088e56d03de0489491eac5f81ed2adc
Mari Muraki <marimuraki@gmail.com>
2014-03-28 17:00:03 -0700

    updated query to include NULL languages

--- bigquery/pushevents_by_yearmonthday.sql
+++ bigquery/pushevents_by_yearmonthday.sql
@@ -1,7 +1,7 @@
 # QUERY: TOTAL PUSHES BY LANGUAGE (YEAR-MONTH-DAY)
 # pushevents_by_yearmonthday
 
-SELECT year(repository_pushed_at) as year, month(repository_pushed_at) as month, dayofweek(repository_pushed_at) as day, repository_language, count(repository_language) AS pushes_by_lang 
+SELECT year(repository_pushed_at) as year, month(repository_pushed_at) as month, dayofweek(repository_pushed_at) as day, repository_language, count(*) AS pushes_by_lang 
 FROM [githubarchive:github.timeline] 
 WHERE type == "PushEvent"
 GROUP EACH BY year, month, day, repository_language

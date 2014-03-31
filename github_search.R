82d7bf9bef113dfe000af2d00d283e1059ece12b
Mari Muraki <marimuraki@gmail.com>
2014-03-30 22:26:58 -0700

    updated graphs

--- github_search.R
+++ github_search.R
@@ -8,8 +8,7 @@ setwd("~/Dropbox/GA/project")
 mydata <- read.csv("github_search.csv") 
 
 # summarize
-summaryBy(list(c("repos_by_lang_t3"), c("conditions", "year")), data=mydata,
-          FUN=c(min,mean,max,sum))
+summaryBy(list(c("repos_by_lang_t3"), c("conditions", "year")), data=mydata, FUN=c(min,mean,max,sum))
 
 # subsets: (1) fork:true (2) fork:false
 forktrue  = subset(mydata, conditions=="fork:true")
@@ -19,21 +18,24 @@ forkfalse = subset(mydata, conditions=="fork:false")
 repos_total <- aggregate(repos_by_lang_t3 ~ conditions + year, data=mydata, FUN=sum)
 
 png('github_search_top10totals.png')
-ggplot(repos_total, aes(year,repos_by_lang_t3,group=conditions,colour=conditions)) + 
+p1 <- ggplot(repos_total, aes(year,repos_by_lang_t3,group=conditions,colour=conditions)) + 
   geom_point() +
   geom_line() +
   xlab("Year") +
   ylab("Total") + 
-  ggtitle("Total Projects & Repos for the \n Top 10 GitHub Languages over Time") +
+  ggtitle("Total Repos & Projects for the \n Top 10 GitHub Languages over Time") +
   scale_x_continuous(breaks=2006:2013) +
   scale_y_continuous(labels=comma) + 
-  scale_colour_discrete(name="Type", labels=c("Projects (excluding forks)", "Repos (including forks)")) +
+  scale_colour_discrete(name="Type", 
+                        breaks=c("fork:true", "fork:false"),
+                        labels=c("Repos (including forks)", "Projects (excluding forks)")) +
   theme(legend.position=(c(.25,.722)))
+p1
 dev.off()
 
 # repos
 png('github_search_top10totals_coloredbylanguage.png')
-ggplot(forktrue, aes(year,repos_by_lang_t3,group=language,colour=language)) + 
+p2 <- ggplot(forktrue, aes(year,repos_by_lang_t3,group=language,colour=language)) + 
   geom_point() +
   geom_line() +
   xlab("Year") +
@@ -42,6 +44,7 @@ ggplot(forktrue, aes(year,repos_by_lang_t3,group=language,colour=language)) +
   scale_x_continuous(breaks=2006:2013) +
   scale_y_continuous(limits=c(0,1000000), labels=comma) +
   scale_colour_discrete(name="Language")
+p2
 dev.off()
 
 png('github_search_top10totals_groupedbylanguage.png')
@@ -56,7 +59,7 @@ dev.off()
 
 # projects
 png('github_search_top10totals_coloredbylanguage_forkfalse.png')
-ggplot(forkfalse, aes(year,repos_by_lang_t3,group=language,colour=language)) + 
+p3 <- ggplot(forkfalse, aes(year,repos_by_lang_t3,group=language,colour=language)) + 
   geom_point() +
   geom_line() +
   xlab("Year") +
@@ -65,6 +68,7 @@ ggplot(forkfalse, aes(year,repos_by_lang_t3,group=language,colour=language)) +
   scale_x_continuous(breaks=2006:2013) +
   scale_y_continuous(limits=c(0,400000), labels=comma) +
   scale_colour_discrete(name="Language")
+p3
 dev.off()
 
 png('github_search_top10totals_groupedbylanguage_forkfalse.png')
@@ -76,4 +80,3 @@ ggplot(forkfalse, aes(year,repos_by_lang_t3)) +
   ggtitle("Total Projects for the \n Top 10 GitHub Languages over Time") +
   scale_y_continuous(limits=c(0,400000), labels=comma)
 dev.off()
-

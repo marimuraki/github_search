96075c38f230fc635381ccf09280095f2805a34f
Mari Muraki <marimuraki@gmail.com>
2014-03-30 17:13:06 -0700

    updated dissimilarity distance & graphs

--- bigquery/pushevents_language_correlations.R
+++ bigquery/pushevents_language_correlations.R
@@ -28,15 +28,11 @@ query_totals <- aggregate(pushes_by_lang~repository_language,
 query_totals <- query_totals[order(-query_totals$pushes_by_lang),]
 colnames(query_totals)[colnames(query_totals)=="pushes_by_lang"] <- "total_by_lang"
 query_totals$rank <- rank(-query_totals$total_by_lang)
-top25langs        <- subset(query_totals, rank <= 25)
 
 # subset to top 25 popular languages
-query_top25langs <- merge(query, top25langs, by="repository_language")
-query_top25langs <- query_top25langs[c(1:3)]
-write.table(query_top25langs, 
-            file="pushevents_by_actor_language_top25.csv", 
-            sep=",", 
-            row.names=FALSE)
+top25langs        <- subset(query_totals, rank <= 25)
+query_top25langs  <- merge(query, top25langs, by="repository_language")
+query_top25langs  <- query_top25langs[c(1:3)]
 
 # reshape wide
 query_top25langs_wide <- reshape(query_top25langs,idvar="actor", 
@@ -56,19 +52,35 @@ colnames_removing_prefix <- function(df, prefix) {
 colnames(query_top25langs_wide) <- colnames_removing_prefix(query_top25langs_wide, "pushes_by_lang.")
 
 # correlation matrix
-png('github_bigquery_pushevents_language_correlations.png')
 correlations <- cor(query_top25langs_wide, method=c("spearman"))
+
+# correlation matrix as level plot
+png('github_bigquery_pushevents_language_correlations.png')
 levelplot(correlations, 
           at=seq(-1,1,.02), 
           scales=list(x=list(rot=90)), 
-          main="Correlation Matrix of\nTop 25 GitHub Languages")
+          xlab="",
+          ylab="",
+          main="Correlation Matrix of\nTop 25 GitHub Languages\n(2013)")
 dev.off()
 
+# identify top 5 (=10/2) correlations
+corrmatrix  <- correlations[-2]
+corrmatrix  <- as.matrix(corrmatrix)
+l           <- length(corrmatrix)
+l1          <- length(corrmatrix[corrmatrix<1])
+corrhigh    <- order(corrmatrix)[(l1-9):l1]
+corrhighonly    <- corrmatrix
+corrhighonly[!1:l %in% corrhigh] <- NA
+corrhighonly
+
 # dissimilarity & hierachichal clustering
-dissimilarity <- 1-correlations
+dissimilarity <- 1-abs(correlations)
 distance      <- as.dist(dissimilarity)
 png('github_bigquery_pushevents_language_clusters.png')
 plot(hclust(distance), 
+     xlab="",
      ylab="Dissimilarity Distance",
-     main="Hierachical Clustering of\nTop 25 Github Languages")
+     main="Hierachical Clustering of\nTop 25 Github Languages\n(2013)")
 dev.off()
+
